bef79938e7cbc6ee3dc9104552b4d56cf3f04e79
Geli Yang <gyang5@villanova.edu>
2022-07-23 12:44:17 -0400

    test Login and SignupForm functionality

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import Title from "antd/lib/skeleton/Title";
+
+const { Header, Content } = Layout;
+
+function App() {
+  const [authed, setAuthed] = useState(false);
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div className="header">
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Lai Food
+          </Title>
+          <div>{authed ? "MyCart Placeholder" : <SignupForm />}</div>
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: "50px",
+          maxHeight: "calc(100% - 64px)",
+          overflowY: "auto",
+        }}
+      >
+        {authed ? (
+          <div>content placeholder</div>
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
