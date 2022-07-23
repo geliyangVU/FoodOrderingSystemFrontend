f47f87e1ecc63733eb31a77f779b556439ee8fc5
Geli Yang <gyang5@villanova.edu>
2022-07-23 12:07:23 -0400

    Import LoginForm to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import { Layout } from "antd";
+import LoginForm from "./components/LoginForm"
 const { Header, Content } = Layout;
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       >
         content
       </Content>
+      <LoginForm></LoginForm>
     </Layout>
   );
 }
